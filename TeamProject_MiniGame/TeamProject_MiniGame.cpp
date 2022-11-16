db17cebdce91344f3cecf9c96031d9caf77d690e
i8768 <i876838@gmail.com>
2022-11-17 04:48:46 +0900

    초기 커밋.

---
+++ TeamProject_MiniGame/TeamProject_MiniGame.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+}
+
+void upgrade369()
+{
+	srand(time(0));
+	int n1, n2, n3;
+}
