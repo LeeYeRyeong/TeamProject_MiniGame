9bdc11f6704e17426179491c929502c2a1af377c
i8768 <i876838@gmail.com>
2022-11-17 14:20:57 +0900

    commit

--- TeamProject_MiniGame/TeamProject_MiniGame.cpp
+++ TeamProject_MiniGame/TeamProject_MiniGame.cpp
@@ -5,12 +5,17 @@ using namespace std;
 
 int main()
 {
-	int n;
-	cin >> n;
+	cout << "        CHOOSE THE GAME" << endl << "------------------------------------" << endl;
+	cout << "1. 369 upgrade.ver" << endl << "2. baseball game" << endl << "3. baskin robbins 31 upgrade.ver"
+		<< endl << "4. subway game" << endl << "5. rock, scissors, paper game" << endl << "6. lotto game" << endl
+		<< "------------------------------------";
 }
 
 void upgrade369()
 {
 	srand(time(0));
 	int n1, n2, n3;
+	n1 = rand() % 9 + 1;
+	n2 = rand() % 9 + 1;
+	n3 = rand() % 9 + 1;
 }
